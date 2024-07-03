bbb20d52aadcc13e8d3ade142970333f929d8c60
julien <morinju@hotmail.fr>
2024-07-03 10:50:44 +0200

    Adding comment in app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require("express");
-const helper = require("./helper.js");
+// Récuperer uniquement la fonction success du module Helper.js
+const {success} = require("./helper.js");
 let pokemons = require("./mock-pokemon.js");
 
 const app = express();
 const port = 3000;
-
+//Point de terminaison test
 app.get("/", (req, res) => {
+    //Message de bienvenue
     res.send("Hello Express  👌   !");
 })
 
+//Point de terminaison permettant de récupérer un pokemon/id
 app.get("/api/pokemons/:id", (req, res) => {
+    // constante pour compter le nombre de pokemons
     const id = parseInt(req.params.id);
+    // constante pour rechercher le bon id
     const pokemon = pokemons.find(pokemon => pokemon.id == id);
+    //constante pour le message
     const message = `Un pokemon a bien été trouvé`;
-    res.json(helper.success(message, pokemon));
+    // Ajout d'un message pour informer que le pokemon a bien été trouvé
+    res.json(success(message, pokemon));
 })
 
+//Le nouveau point de terminaison affiche le nombre de pokemons se trouvant dans le Pokedex
 app.get(`/api/pokemons`, (req, res) => {
     res.send(`Il y a ${pokemons.length} pokemons dans le Pokedex pour le moment.`);
 })
