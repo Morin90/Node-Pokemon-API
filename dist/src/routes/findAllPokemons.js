17e0f9f0a8e53a4c0aef7cba0aae3a6c6be43054
julien <morinju@hotmail.fr>
2024-07-09 11:43:51 +0200

    Add advanced search in findAllPokemon, limit

--- dist/src/routes/findAllPokemons.js
+++ dist/src/routes/findAllPokemons.js
@@ -17,11 +17,26 @@ const express_1 = require("express");
 const router = (0, express_1.Router)();
 router.get('/api/pokemons', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const pokemons = yield pokemons_1.default.find(); // Récupérer tous les Pokémon
-        res.json({
-            message: 'Les Pokémon ont bien été récupérés.',
-            data: pokemons
-        });
+        // Recherche des pokemons par nom
+        const limit = parseInt(req.query.limit) || 6; // limiter la recherche a 6 pokemons maximum
+        if (req.query.name) {
+            const name = req.query.name;
+            const totalPokemons = yield pokemons_1.default.countDocuments({ name: new RegExp(name, 'i') });
+            const pokemons = yield pokemons_1.default.find({ name: new RegExp(name, 'i') }).sort({ name: 1 }).limit(limit); // Utilisation de regex pour recherche insensible à la casse
+            return res.json({
+                message: `Il y a ${totalPokemons} pokemons qui correspondent è votre recherche.`,
+                data: pokemons
+            });
+        }
+        else {
+            // Compter tous les Pokémon dans la base de données
+            const totalPokemons = yield pokemons_1.default.countDocuments();
+            const pokemons = yield pokemons_1.default.find().sort({ name: 1 }); // Récupérer tous les Pokémon
+            res.json({
+                message: 'Les Pokémon ont bien été récupérés, il y en a ' + totalPokemons + ' au total.',
+                data: pokemons
+            });
+        }
     }
     catch (error) {
         res.status(500).json({
--- src/routes/findAllPokemons.ts
+++ src/routes/findAllPokemons.ts
@@ -7,11 +7,25 @@ const router = Router();
 
 router.get('/api/pokemons', async (req: Request, res: Response) => {
     try {
-        const pokemons = await Pokemon.find(); // Récupérer tous les Pokémon
-        res.json({
-            message: 'Les Pokémon ont bien été récupérés.',
-            data: pokemons
-        });
+        // Recherche des pokemons par nom
+        const limit = parseInt(req.query.limit as string) || 6; // limiter la recherche a 6 pokemons maximum
+        if (req.query.name) {
+            const name = req.query.name as string;
+            const totalPokemons = await Pokemon.countDocuments({ name: new RegExp(name, 'i') });
+            const pokemons = await Pokemon.find({ name: new RegExp(name, 'i') }).sort({ name: 1 }).limit(limit); // Utilisation de regex pour recherche insensible à la casse
+            return res.json({
+                message: `Il y a ${totalPokemons} pokemons qui correspondent è votre recherche.`,
+                data: pokemons
+            });
+        } else {
+            // Compter tous les Pokémon dans la base de données
+            const totalPokemons = await Pokemon.countDocuments();
+            const pokemons = await Pokemon.find().sort({ name: 1 }); // Récupérer tous les Pokémon
+            res.json({
+                message: 'Les Pokémon ont bien été récupérés, il y en a ' + totalPokemons + ' au total.',
+                data: pokemons
+            });
+        }
     } catch (error: any) {
         res.status(500).json({
             message: 'Une erreur est survenue lors de la récupération des Pokémon.',
